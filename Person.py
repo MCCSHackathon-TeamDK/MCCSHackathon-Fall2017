3259ffa0ac95179c15d11ce902562d0af253c581
dgrossmann144 <dgrossmann144@gmail.com>
2017-12-01 20:25:22 -0500

    Added function to generate list of people to give a gift to

--- Person.py
+++ Person.py
@@ -3,7 +3,7 @@ import json
 
 class Person:
 
-    def __init__(self, name, id=None, age=None, relationship=None):
+    def __init__(self, name, age=None, relationship=None):
         """
         Initializes the person class with a person's name, age, and relationship.
         This will only be used when creating a new person that has not been added yet, as people who have already been
@@ -12,29 +12,27 @@ class Person:
         :param age: Age of this person
         :param relationship: Relationship of this person to the user
         """
-        if id is not None:
-            # If an ID was passed we know we are creating a new person
-            self.id = id
+        if age is not None and relationship is not None:
             self.name = name
             self.age = age
             self.relationship = relationship
             self.no_gift_rule = False
             self.gifts = []
             self.interests = []
-            self.family = []  # List of tuples (relationship to this person, their person id)
+            self.family = []  # List of tuples (relationship to this person, their person object)
         else:
             self.load_person(name)
 
-    def save_person(self, base_directory):
+    def save_person(self):
         """
         Saves this person to a JSON file
         :return:
         """
-        with open(base_directory + "/" + self.name + '.txt', 'w') as outfile:
+        with open(self.name + '.txt', 'w') as outfile:
             json.dump(self.__dict__, outfile)
 
     def load_person(self, name):
-        with open(name) as json_file:
+        with open(name + ".txt") as json_file:
             json_dict = json.load(json_file)
 
             self.name = json_dict["name"]
--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from datetime import datetime
 
 def main():
     test_guy = Person("tommy", age=11, relationship="friend")
+    test_guy.add_gift(Gift("meme", 100, 2015, 144))
+    test_guy.add_gift(Gift("another meme", 20, 2016, 145))
+    test2 = Person("bob", age=50, relationship="father")
+    test2.add_gift(Gift("Test", 2, 2016, 146))
+    test2.add_gift(Gift("2test", 50, 2011, 147))
+    testlist = [test_guy, test2]
+
+    for person in generate_gift_list(testlist, 2010, 25):
+        print(person.name)
 
     test_guy.save_person()
 
@@ -15,11 +24,6 @@ def main():
     print(test_again.interests)
     print(test_again.no_gift_rule)
 
-    print("stuff")
-    print(send_card(test_guy))
-    test_guy.add_gift(Gift("christmas card", 0, 2016, 144))
-    print(send_card(test_guy))
-
 def send_card(person):
     """
     Checks if person sent a card last year
@@ -29,6 +33,15 @@ def send_card(person):
         if gift.is_card() and gift.year == year - 1 and not person.no_gift_rule:
             return True
     return False
+
+def generate_gift_list(people, year_given, price_limit):
+    result = list()
+    for person in people:
+        for gift in person.gifts:
+            if gift.year > year_given and gift.price > price_limit and person not in result and not person.no_gift_rule:
+                result.append(person)
+
+    return result
 
 if __name__ == "__main__":
     main()
