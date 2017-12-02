c90373b298b573eec2437e0c05a020be01baa55c
WilliamKluge <klugewilliam@gmail.com>
2017-12-01 19:04:42 -0500

    added more to person class

--- Person.py
+++ Person.py
@@ -1,6 +1,9 @@
+import json
+
+
 class Person:
 
-    def __init__(self, name, age, relationship=""):
+    def __init__(self, name, age=None, relationship=None):
         """
         Initializes the person class with a person's name, age, and relationship.
         This will only be used when creating a new person that has not been added yet, as people who have already been
@@ -9,20 +12,27 @@ class Person:
         :param age: Age of this person
         :param relationship: Relationship of this person to the user
         """
-        self.name = name
-        self.age = age
-        self.relationship = relationship
-        self.no_gift_rule = False
-        self.gifts = []
-        self.interests = []
-        self.family = []  # List of tuples (relationship to this person, their person object)
+        if age is None or relationship is None:
+            self.name = name
+            self.age = age
+            self.relationship = relationship
+            self.no_gift_rule = False
+            self.gifts = []
+            self.interests = []
+            self.family = []  # List of tuples (relationship to this person, their person object)
+        else:
+
 
     def save_person(self):
         """
         Saves this person to a JSON file
         :return:
         """
-        pass
+        with open(self.name + '.txt', 'w') as outfile:
+            json.dump(self, outfile)
+
+    def load_person(self, name):
+        json_dict = json.load(name)
 
     def add_family_member(self, role, person):
         """
